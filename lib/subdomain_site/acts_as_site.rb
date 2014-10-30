a08ce2b08b199cca562e5b64d9a161e670eb1d8a
Johannes Müller <straightshoota@gmail.com>
2014-10-30 17:42:41 +0100

    refactored acts_as_site using active record callback

--- lib/subdomain_site/acts_as_site.rb
+++ lib/subdomain_site/acts_as_site.rb
@@ -3,33 +3,22 @@ module SubdomainSite
     extend ActiveSupport::Concern
 
     module ClassMethods
+
       def acts_as_site(options = {})
         include SubdomainSite::ActsAsSite::LocalInstanceMethods
 
         options = { subdomain_attr: options } unless options.is_a?(Hash)
         options[:subdomain_attr] ||= :subdomain
-        @subdomain_attr = options[:subdomain_attr].to_sym
 
-        class_eval do
-          require 'active_model'
-          include ActiveModel::Validations
+        cattr_accessor :subdomain_attr
+        self.subdomain_attr = options[:subdomain_attr].to_sym
 
-          validates @subdomain_attr, subdomain_attr_validations
+        require 'active_model'
+        include ActiveModel::Validations
+        include ActiveModel::Validations::Callbacks
 
-          if constants.include?(:ActiveRecord) && include?(::ActiveRecord::Base)
-            instance_eval "def #{@subdomain_attr}= (value); write_attribute(#{@subdomain_attr}, filter_subdomain_value(value)); end"
-          end
-
-          def find_by_subdomain(subdomain, params = {})
-            subdomain.downcase!
-
-            # TODO: enable some kind of caching
-
-            find_by(params.merge(@subdomain_attr => subdomain))
-          end
-        end
-
-        class_eval "def to_param; filter_subdomain_value(#{@subdomain_attr}); end"
+        validates self.subdomain_attr, subdomain_attr_validations
+        before_validation :downcase_subdomain
 
         # There should usually only be one model representing subsites
         # but this automatic setting interferes with test suites.
@@ -44,6 +33,11 @@ module SubdomainSite
                }
         vals[:uniqueness] = { case_sensitive: false } if respond_to? :validates_uniqueness_of
         vals
+      end
+
+      def find_subdomain_site(subdomain, params = {})
+        # TODO: enable some kind of caching
+        find_by(params.merge(self.subdomain_attr => subdomain.downcase))
       end
     end
 
@@ -66,6 +60,14 @@ module SubdomainSite
 
       def filter_subdomain_value(val)
         val.to_s.downcase unless val.nil?
+      end
+
+      def downcase_subdomain
+        send "#{self.class.subdomain_attr}=", filter_subdomain_value(send "#{self.class.subdomain_attr}")
+      end
+
+      def to_param
+        filter_subdomain_value send self.class.subdomain_attr
       end
     end
   end
