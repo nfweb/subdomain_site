43a724916d09ca6b33ffa5d77e8ba3364088359b
Johannes Müller <straightshoota@gmail.com>
2014-11-05 18:09:42 +0100

    refactored url_for for simplification

--- lib/subdomain_site/url_for.rb
+++ lib/subdomain_site/url_for.rb
@@ -1,28 +1,20 @@
 module SubdomainSite
   module UrlFor
-    # Makes url_for(site: 'admin') the same as url_for(subdomain: 'admin', only_path: false)
     def url_for(options, route_name = nil, url_strategy = nil)
       options = options.dup
 
-      url_strategy = ActionDispatch::Routing::RouteSet::UNKNOWN if SubdomainSite::RAILS42 && url_strategy.nil?
+      url_strategy ||= ActionDispatch::Routing::RouteSet::UNKNOWN if SubdomainSite::RAILS42
 
       if options.key?(:site)
         site = options.delete(:site)
 
-        site_options = site.default_url_options if site.respond_to? :default_url_options
-        site_options ||= { subdomain: site.to_param }
+        options[:subdomain] = site.to_param
 
-        options.reverse_merge! site_options
-
-        if options.key? :only_path
-          options[:only_path] = current_site?(site) && options[:only_path]
-        end
-
-        # if site specified force full URL
+        # force full URL if site differs from current
         if SubdomainSite::RAILS42
           url_strategy = ActionDispatch::Routing::RouteSet::FULL unless current_site?(site)
         else
-          options[:only_path] ||= false unless current_site?(site)
+          options[:only_path] = false unless current_site?(site)
         end
       end
 
@@ -47,41 +39,39 @@ class ActionDispatch::Routing::RouteSet::NamedRouteCollection
   class UrlHelper
     if SubdomainSite::RAILS42
       def handle_positional_args_with_site(controller_options, inner_options, args, result, path_params)
-        if args.first.respond_to?(:site_member?) && args.first.site_member?
-          result[:site] = args.first.site
-          args.shift if args.first.respond_to?(:site?) && args.first.site?
-        end
+        result.merge! prepare_site_options(args)
         handle_positional_args_without_site(controller_options, inner_options, args, result, path_params)
       end
     else
       def handle_positional_args_with_site(t, args, options, keys)
         options = options.dup
-        if args.first.respond_to?(:site_member?) && args.first.site_member?
-          options[:site] = args.first.site
-          args.shift if args.first.respond_to?(:site?) && args.first.site?
-        end
+        options.merge prepare_site_options(args)
         handle_positional_args_without_site(t, args, options, keys)
       end
     end
+
+    def prepare_site_options(args)
+      return {} unless args.first.respond_to?(:site_member?) && args.first.site_member?
+
+      site = args.first.site
+
+      # remove element from args list if it is a site itself
+      args.shift if args.first.respond_to?(:site?) && args.first.site?
+
+      site_options = site.default_url_options if site.respond_to? :default_url_options
+      site_options ||= {}
+
+      site_options[:site] = site
+      site_options
+    end
+
+    private :prepare_site_options
 
     alias_method_chain :handle_positional_args, :site
 
     def self.optimize_helper?(_route)
       # TODO: find implementation for optimized helper. maybe decorate ```t``?
       false
-    end
-  end
-
-  class OptimizedUrlHelper
-    def call(t, args)
-      if args.size == arg_size && !args.last.is_a?(Hash) && optimize_routes_generation?(t)
-        options = @options.dup
-        options.merge!(t.url_options) if t.respond_to?(:url_options)
-        options[:path] = optimized_helper(args)
-        ActionDispatch::Http::URL.url_for(options)
-      else
-        super
-      end
     end
   end
 end
